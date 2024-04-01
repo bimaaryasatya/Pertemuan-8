21742288fab5921a1c2480e131c9133ca009c0e3
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2024-04-01 22:00:56 +0700

    Update Homework2.py

--- Homework/Homework2.py
+++ Homework/Homework2.py
@@ -3,13 +3,15 @@ nama = str(input("Nama : "))
 kode = str(input("Kode Booking : "))
 NIK = int(input("NIK : "))
 Nomor_Kursi = str(input("Nomor Kursi : "))
-Tanggal = str(input("Tanggal Pemberangkatan : "))
-Waktu =  input("Jam Keberangkatan : ")
+Tanggal = int(input("Tanggal Pemberangkatan : "))
+Waktu =  float(input("Jam Keberangkatan : "))
 
 data_tiket = ["AMT39R"]
 data_nama = ["UDIN"]
 data_NIK = [33281234567]
 data_nokursi = ["4A"]
+tanggal_budhal = [19]
+waktu_budhal = [20.00]
 
-data = "Diperbolehkan Masuk" if kode in data_tiket and nama in data_nama and  NIK in data_NIK and Nomor_Kursi in data_nokursi else "Tidak Sesuai"
+data = "Diperbolehkan Masuk" if kode in data_tiket and nama in data_nama and  NIK in data_NIK and Nomor_Kursi in data_nokursi and Tanggal in tanggal_budhal and Waktu in waktu_budhal else "Tidak Sesuai"
 print(data)
