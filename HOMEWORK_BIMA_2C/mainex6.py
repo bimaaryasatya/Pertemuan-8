7a902337260d69256d32b193677578e75bab75be
bimaaryasatya <bimarailfansslw69@gmail.com>
2024-05-18 15:25:45 +0700

    add

---
+++ HOMEWORK_BIMA_2C/mainex6.py
@@ -0,0 +1,24 @@
+import exercase6 as ex6
+
+print(f'{5*"="}Selamat Datang Di Program Manajemen Barang{5*"="}')
+print("Masukkan Pilihan\n1.Tambah Barang\n2.Cari Barang\n3.Hapus Barang\n4.Edit Barang\n5.Tampilkan Data Barang\n6.Tampilkan Jumlah Data\n7.Keluar")
+
+while True:
+    pilihan = int(input('Masukkan Pilihan: '))
+    if pilihan == 1:
+        ex6.sum()
+    elif pilihan == 2:
+        ex6.search()
+    elif pilihan == 3:
+        ex6.delete()
+    elif pilihan == 4:
+        ex6.edit()
+    elif pilihan == 5:
+        print(f"{10*'='}Toko Elektronik{10*'='}")
+        ex6.view_data()
+    elif pilihan == 6:
+        ex6.view_item_cout()
+    elif pilihan == 7:
+        print("Terima Kasih Telah Menggunakan Program Kami")
+        break
+
